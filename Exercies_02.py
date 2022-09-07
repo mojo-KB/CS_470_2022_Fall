fa5d9d6bcfbde3baf98adf7cf647ae9bb22a1119
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-09-07 18:57:43 -0400

    Create exercies_02.py

---
+++ Exercies_02.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+image = np.zeros((480, 640, 3), dtype="uint8")
+
+print("Hello")
+
+a = [0, 10, 20, 30, 40, 50]
+print(a[1:3])
+image[:,:] = (0,0,255)
+
+
+cv2.imshow("Image", image)
+cv2.waitKey(-1)
+cv2.destroyAllWindows()
+
+
+# import tensorflow as tf
+
+# print(tf.__version__)
