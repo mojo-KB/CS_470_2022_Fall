0ca29b518785d62a7ae3927b7c834b176091ccd2
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-09-07 19:13:40 -0400

    Update Exercises_02

--- Exercies_02.py
+++ Exercies_02.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 
 image = np.zeros((480, 640, 3), dtype="uint8")
-image2 = np.zeros((480, 640, 3), dtype="unit8")
+#image2 = np.zeros((480, 640, 3), dtype="unit8")
 print("Hello")
 
 a = [0, 10, 20, 30, 40, 50]
 print(a[1:3])
 image[:100,:] = (0,0,255)
-image2[:200,:]  = (255,0,0)
+#image2[:200,:]  = (255,0,0)
 
 cv2.imshow("Image", image)
 cv2.waitKey(-1)
