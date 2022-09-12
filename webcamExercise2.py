69c3da8087e1033b503a7a24ccf857cbb25228aa
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-09-12 19:06:00 -0400

    Update webcamExercise2.py

--- webcamExercise2.py
+++ webcamExercise2.py
@@ -29,9 +29,29 @@ import pandas
 import sklearn
 import sys
 
-###############################################################################
-# MAIN
-###############################################################################
+
+
+counter = 0
+MAX_COUNTER = 30
+last_image = None
+
+
+def process(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    global counter 
+    global last_image
+
+    counter += 1
+    if last_image is None:
+        last_iamge = np.copy(image).astype("float32")
+    
+    if counter >= MAX_COUNTER:
+        counter = 0
+        last_image = np.copy(image).astype("float32")
+
+    image = cv2.convertScaleAbs(image.astype("float32")*0.5 + last_image*0.5)
+    return image
+
 
 def main():
     ###############################################################################
@@ -125,58 +145,3 @@ if __name__ == "__main__":
 
 
 
-counter = 0
-MAX_COUNTER = 30
-last_image = None
-
-
-def process(image):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2XYZ)
-    global counter 
-    global last_image
-
-    counter += 1
-    if last_image is None:
-        last_iamge = np.copy(image).astype("float32")
-    
-    if counter >= MAX_COUNTER:
-        counter = 0
-        last_image = np.copy(image).astype("float32")
-
-    image = cv2.convertScaleAbs(image.astype("float32")*0.5 + last_iamge*0.5)
-
-
-    # minVal = 100
-    # maxVal = 200
-    # image = np.where(image <= minVal, minVal, image)
-    # image = np.where(image >= maxVal, maxVal, image)
-    return image
-
-myimage = cv2.imread("test.jpg")
-myimage = process(myimage)
-
-
-
-# image = np.zeros((480, 640, 3), dtype="uint8")
-# #image2 = np.zeros((480, 640, 3), dtype="unit8")
-# print("Hello")
-
-# a = [0, 10, 20, 30, 40, 50]
-# print(a[1:3])
-# image[:100,:] = (0,0,255)
-# #image2[:200,:]  = (255,0,0)
-# image[100:200,:] = 128
-# #image[:,:,0:,1] = 128
-
-# myimage = cv2.resize(myimage, dsize=(0,0), fx=0.1, fy=0.1)
-# myimage = cv2.resize(myimage, dsize=(0,0), fx=10.0, fy=10.0, interpolation=cv2.INTER_NEAREST)
-
-# cv2.imshow("Image", image)
-# cv2.imshow("Image2", myimage);
-# cv2.waitKey(-1)
-# cv2.destroyAllWindows()
-
-
-# import tensorflow as tf
-
-# print(tf.__version__)
